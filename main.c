bdd023603656fb7144044cadb6fdd6404a118b3d
Julio Meroño Sáez <segfaultdev@proton.me>
2023-10-16 23:33:31 +0200

    FloriQuiz: Show trivia text.

--- main.c
+++ main.c
@@ -35,6 +35,26 @@ void lcd_send(uint data, bool rs) {
   sleep_ms(2);
 }
 
+void lcd_hispanify(void) {
+  lcd_send(0x40, false);
+  
+  const unsigned char font[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    
+    0x02, 0x04, 0x00, 0x0E, 0x01, 0x0F, 0x11, 0x0F,
+    0x02, 0x04, 0x00, 0x0E, 0x11, 0x1F, 0x10, 0x0E,
+    0x02, 0x04, 0x00, 0x04, 0x0C, 0x04, 0x04, 0x0E,
+    0x02, 0x04, 0x00, 0x0E, 0x11, 0x11, 0x11, 0x0E,
+    0x02, 0x04, 0x00, 0x11, 0x11, 0x11, 0x13, 0x0D,
+  };
+  
+  for (uint i = 0; i < 8 * 8; i++) {
+    lcd_send(font[i], true);
+  }
+  
+  lcd_send(0x80, false);
+}
+
 void lcd_clear(void) {
   lcd_send(0x01, false);
 }
@@ -47,10 +67,10 @@ void lcd_goto(uint x, uint y) {
   lcd_send(0x80 + (x + y * 64), false);
 }
 
-void lcd_write(const char *str) {
-  while (*str) {
-    lcd_send(*str, true);
-    str++;
+void lcd_write(const char *text) {
+  while (*text) {
+    lcd_send(*text, true);
+    text++;
   }
 }
 
@@ -65,19 +85,90 @@ void input_refresh(void) {
   }
 }
 
-void trivia_
+uint trivia_write(const char *text) {
+  uint count = 0;
+  
+  while (*text) {
+    if ((unsigned char)(*text) == 0xC2) {
+      text++;
+      
+      if ((unsigned char)(*text) == 0xBF) {
+        lcd_send(6, true);
+      }
+    } else if ((unsigned char)(*text) == 0xC3) {
+      text++;
+      
+      if ((unsigned char)(*text) == 0xB1) {
+        lcd_send(0, true);
+      } else if ((unsigned char)(*text) == 0xA1) {
+        lcd_send(1, true);
+      } else if ((unsigned char)(*text) == 0xA9) {
+        lcd_send(2, true);
+      } else if ((unsigned char)(*text) == 0xAD) {
+        lcd_send(3, true);
+      } else if ((unsigned char)(*text) == 0xB3) {
+        lcd_send(4, true);
+      } else if ((unsigned char)(*text) == 0xBA) {
+        lcd_send(5, true);
+      }
+    } else {
+      lcd_send(*text, true);
+    }
+    
+    count++, text++;
+    sleep_ms(20);
+  }
+  
+  return count;
+}
+
+void trivia_show_option(const char *text) {
+  lcd_send(0x0D, false);
+  lcd_goto(0, 1);
+  
+  uint count = trivia_write(text);
+  
+  lcd_send(0x0C, false);
+  sleep_ms(1000);
+  
+  lcd_send(0x0D, false);
+  lcd_goto(0, 1);
+  
+  while (count--) {
+    lcd_send(' ', true);
+    sleep_ms(20);
+  }
+  
+  lcd_send(0x0C, false);
+}
+
+void trivia_show(const trivia_t trivia) {
+  lcd_send(0x0D, false);
+  lcd_clear();
+  
+  trivia_write(trivia.text);
+  
+  lcd_send(0x0C, false);
+  sleep_ms(2000);
+  
+  for (uint i = 0; i < 4; i++) {
+    trivia_show_option(trivia.options_long[i]);
+  }
+  
+  sleep_ms(2000);
+}
 
 int main(void) {
   uint offset = pio_add_program(pio0, &neopixel_program);
   neopixel_program_init(pio0, 0, offset, 22, 1000000, false);
   
   for (int i = 0; i < 10; i++) {
-    pio_sm_put_blocking(pio0, 0, 0x20000000);
-    pio_sm_put_blocking(pio0, 0, 0x20200000);
-    pio_sm_put_blocking(pio0, 0, 0x00200000);
-    pio_sm_put_blocking(pio0, 0, 0x00202000);
-    pio_sm_put_blocking(pio0, 0, 0x00002000);
-    pio_sm_put_blocking(pio0, 0, 0x20002000);
+    pio_sm_put_blocking(pio0, 0, 0xFF000000);
+    pio_sm_put_blocking(pio0, 0, 0xFFFF0000);
+    pio_sm_put_blocking(pio0, 0, 0x00FF0000);
+    pio_sm_put_blocking(pio0, 0, 0x00FFFF00);
+    pio_sm_put_blocking(pio0, 0, 0x0000FF00);
+    pio_sm_put_blocking(pio0, 0, 0xFF00FF00);
   }
   
   gpio_init_mask(LCD_MASK);
@@ -100,15 +191,15 @@ int main(void) {
   sleep_ms(100);
   
   lcd_send(0x38, false);
-  lcd_send(0x0D, false);
+  lcd_send(0x0C, false);
   lcd_send(0x06, false);
+  
+  lcd_hispanify();
   
   /* --- */
   
-  lcd_clear();
-  lcd_write("FloriQuiz! ");
-  
   while (true) {
+    /*
     if (input_pressed(0)) {
       lcd_write("A");
     }
@@ -122,10 +213,14 @@ int main(void) {
     }
     
     if (input_pressed(3)) {
-      lcd_write("D");
+      // lcd_write("D");
+      lcd_send(0x18, false);
     }
     
     input_refresh();
+    */
+    
+    trivia_show(trivia_set_a[0]);
   }
   
   return 0;
